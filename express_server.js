07525264e4918c5cd853a27b51aaaf534aa0c7b3
chazdean <chaz.dean@shopify.com>
2021-08-28 22:48:44 +0000

    Add random number generator & new post request

--- express_server.js
+++ express_server.js
@@ -7,6 +7,10 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const generateRandomString = () => {
+  return Math.floor(Math.random() * 6000) + 1;
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -27,6 +31,11 @@ app.get("/urls", (req,res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req,res) => {
